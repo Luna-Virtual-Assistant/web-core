b42956be5170ce4d4a595fa75e537e5768544c17
Vitor <Vitor Rafael>
2024-09-10 21:12:57 -0300

    refactor: handle duplicated contacts

--- src/app/chat/_components/chat-page.tsx
+++ src/app/chat/_components/chat-page.tsx
@@ -1,55 +1,104 @@
 'use client'
 
-import { Message } from '@/components/message'
-import {
-  Page,
-  PageFooter,
-  PageHeader,
-  PageHeaderTitle,
-  PageMain,
-} from '@/components/page'
-import { Button } from '@/components/ui/button'
-import { Form } from '@/components/ui/form'
-import { Input } from '@/components/ui/input'
-import { FaArrowUp } from 'react-icons/fa'
-import { useChatPage } from './chat-page.hooks'
+import { MessageProps } from '@/components/message'
+import mqtt, { MqttClient } from 'mqtt'
+import { useCallback, useEffect, useRef, useState } from 'react'
+import { useForm } from 'react-hook-form'
 
-export function ChatPage() {
-  const { messages, messagesEndRef, form, onSubmit } = useChatPage()
+export function useChatPage() {
+  const form = useForm()
+  const [client, setClient] = useState<MqttClient | null>(null)
+  const [messages, setMessages] = useState<MessageProps[]>([])
+  const [isDuplicated, setIsDuplicated] = useState(false)
+  const messagesEndRef = useRef<HTMLDivElement>(null)
 
-  return (
-    <Page className="flex flex-col">
-      <PageHeader className="sticky top-0 z-10">
-        <PageHeaderTitle>Converse com Luna</PageHeaderTitle>
-      </PageHeader>
+  useEffect(() => {
+    const mqttConnect = () => {
+      const newClient = mqtt.connect({
+        keepalive: 60,
+        clientId: 'mqttjs_' + Math.random().toString(16).substr(2, 8),
+        port: 8884,
+        hostname: 'localhost',
+      })
+      setClient(newClient)
+      return newClient
+    }
 
-      <PageMain className="max-w-3xl relative w-full mx-auto flex flex-col h-full overflow-y-auto p-0 py-4">
-        <section className="flex flex-col gap-6 pb-20">
-          {messages.map((message) => (
-            <Message key={message.timestamp} message={message} />
-          ))}
-          <div ref={messagesEndRef} />
-        </section>
+    const newClient = mqttConnect()
 
-        <PageFooter className="fixed bottom-0 w-full bg-muted flex flex-col justify-center border-border max-w-3xl pb-2">
-          <Form {...form}>
-            <form onSubmit={onSubmit} className="flex items-center gap-1">
-              <Input
-                type="text"
-                placeholder="Digite uma mensagem"
-                className="py-6 bg-background rounded-md"
-                {...form.register('message')}
-              />
-              <Button type="submit" className="py-6">
-                <FaArrowUp className="w-4 h-4" />
-              </Button>
-            </form>
-          </Form>
-          <p className="text-sm ml-0.5 mt-2 text-muted-foreground text-medium">
-            Pressione Enter para enviar
-          </p>
-        </PageFooter>
-      </PageMain>
-    </Page>
+    newClient.on('connect', () => {
+      newClient.subscribe('/history/message', (err) => {
+        if (err) {
+          console.error(err)
+        }
+      })
+
+      newClient.subscribe('/luna/web/duplicated', (err) => {
+        if (err) {
+          console.error(err)
+        }
+      })
+
+      newClient.on('message', (topic, message) => {
+        if (topic === '/luna/web/duplicated') {
+          setIsDuplicated(true)
+          return
+        }
+
+        const payload: MessageProps = {
+          body: message.toString(),
+          kind: 'received',
+          timestamp: Date.now(),
+        }
+        setMessages((prev) => [...prev, payload])
+      })
+    })
+
+    return () => {
+      newClient.off('connect', () => {})
+      newClient.end()
+    }
+  }, [])
+
+  const handleSendMessage = useCallback(
+    (message: string) => {
+      if (client) {
+        if (isDuplicated) {
+          client.publish('/luna/select_contact', message)
+          setIsDuplicated(false)
+        } else {
+          client.publish('/luna', message)
+        }
+        const newMessage: MessageProps = {
+          body: message,
+          kind: 'sent',
+          timestamp: Date.now(),
+        }
+        setMessages((prev) => [...prev, newMessage])
+      }
+    },
+    [client, isDuplicated],
   )
+
+  const onSubmit = form.handleSubmit(async (data) => {
+    try {
+      handleSendMessage(data.message)
+      form.resetField('message')
+    } catch (error) {
+      console.error(error)
+    }
+  })
+
+  useEffect(() => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behavior: 'smooth' })
+    }
+  }, [messages])
+
+  return {
+    form,
+    messages,
+    messagesEndRef,
+    onSubmit,
+  }
 }
